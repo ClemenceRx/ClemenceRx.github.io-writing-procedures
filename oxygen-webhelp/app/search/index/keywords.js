3ba6b75f1f70988310ddf6588b26f9984fde06fb
ClemenceRx <47216185+ClemenceRx@users.noreply.github.com>
2021-05-03 14:48:47 -0400

    Add files via upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,31 @@
+define(function() {var keywords=[{w:"Create",p:["p0","p4"]},{w:"glossary",p:["p0"]},{w:"topics",p:["p0","p2","p3","p6","p11","p12","p17"]},{w:"Add",p:["p1","p2","p11","p12","p15"]},{w:"conditions",p:["p1","p13"]},{w:"to",p:["p1","p2","p12","p15"]},{w:"the",p:["p1","p2","p13","p15"]},{w:"content",p:["p1","p3"]},{w:"DITA",p:["p2","p4","p15"]},{w:"map",p:["p2","p4","p15"]},{w:"Insert",p:["p3","p13"]},{w:"reusable",p:["p3","p5","p14"]},{w:"into",p:["p3","p11"]},{w:"a",p:["p4","p7","p8","p9","p10","p15"]},{w:"Isolate",p:["p5","p14"]},{w:"terms",p:["p5"]},{w:"Edit",p:["p6"]},{w:"Generate",p:["p7","p8","p9","p22"]},{w:"PDF",p:["p7","p8","p10","p22"]},{w:"from",p:["p7","p8","p9"]},{w:"ditamap",p:["p7","p9"]},{w:"file",p:["p7","p8","p9"]},{w:"markdown",p:["p8"]},{w:"webhelp",p:["p9","p22"]},{w:"Update",p:["p10"]},{w:"part",p:["p10"]},{w:"numbers",p:["p10"]},{w:"for",p:["p10"]},{w:"each",p:["p10"]},{w:"revision",p:["p10"]},{w:"of",p:["p10","p12"]},{w:"document",p:["p10"]},{w:"conditional",p:["p11","p12"]},{w:"text",p:["p11","p12"]},{w:"parts",p:["p12"]},{w:"in",p:["p13"]},{w:"transformation",p:["p13"]},{w:"steps",p:["p14"]},{w:"title",p:["p15"]},{w:"Troubleshooting",p:["p16"]},{w:"Write",p:["p17"]},{w:"Deliver",p:["p18"]},{w:"outputs",p:["p18","p22"]},{w:"Directories",p:["p19"]},{w:"Naming",p:["p20"]},{w:"conventions",p:["p20"]},{w:"Before",p:["p21"]},{w:"you",p:["p21"]},{w:"start",p:["p21"]},{w:"and",p:["p22"]},{w:"Writing",p:["p23"]},{w:"guidelines",p:["p23"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[3, 2, 5, 6, 8, 9];
+ph["p3"]=[10, 11, 7, 12, 2];
+ph["p4"]=[0, 13, 8, 9];
+ph["p5"]=[14, 11, 15];
+ph["p6"]=[16, 2];
+ph["p7"]=[17, 13, 18, 19, 13, 20, 21];
+ph["p8"]=[17, 13, 18, 19, 13, 22, 21];
+ph["p9"]=[17, 13, 23, 19, 13, 20, 21];
+ph["p10"]=[24, 25, 26, 27, 28, 29, 30, 13, 18, 31];
+ph["p21"]=[46, 47, 48];
+ph["p20"]=[44, 45];
+ph["p12"]=[3, 32, 33, 5, 34, 30, 2];
+ph["p23"]=[50, 51];
+ph["p11"]=[3, 32, 33, 12, 2];
+ph["p22"]=[17, 18, 49, 23, 42];
+ph["p14"]=[14, 11, 37];
+ph["p13"]=[10, 4, 35, 6, 36];
+ph["p16"]=[39];
+ph["p15"]=[3, 13, 38, 5, 6, 8, 9];
+ph["p18"]=[41, 42];
+ph["p17"]=[40, 2];
+ph["p19"]=[43];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {12:17,2:17,1:17,8:22,18:-1,11:17,13:17,6:17,9:22,0:17,3:23,14:23,19:23,4:17,20:23,16:-1,21:-1,17:-1,22:-1,5:23,10:23,23:-1,7:22,15:17};
+});
